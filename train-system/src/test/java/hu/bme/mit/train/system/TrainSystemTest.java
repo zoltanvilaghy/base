b910c6e05ae1316592ab3a83febd9bba81e4bd1d
zoltanvilaghy <61561617+zoltanvilaghy@users.noreply.github.com>
2021-02-25 10:33:29 +0100

    Added new test
    Added new test to check if Tachograph works correctly.

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.train.system;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +10,11 @@ import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.system.TrainSystem;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.lang.Thread.sleep;
 
 public class TrainSystemTest {
 
@@ -73,6 +80,33 @@ public class TrainSystemTest {
 		Assert.assertEquals(5, controller.getReferenceSpeed());
 		controller.followSpeed();
 		Assert.assertEquals(5, controller.getReferenceSpeed());
+	}
+
+	@Test
+	public void AddingValuesToTachograph() {
+		Table<String, Integer, Integer> Tachograph = HashBasedTable.create();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+		sensor.overrideSpeedLimit(10);
+		user.overrideJoystickPosition(5);
+		controller.followSpeed();
+
+		Date date1 = new Date();
+		Tachograph.put(formatter.format(date1), user.getJoystickPosition(), controller.getReferenceSpeed());
+
+		try {
+			sleep(5_000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		controller.followSpeed();
+
+		Date date2 = new Date();
+		Tachograph.put(formatter.format(date2), user.getJoystickPosition(), controller.getReferenceSpeed());
+
+		Assert.assertNotEquals(0, Tachograph.size());
+		Assert.assertEquals(2, Tachograph.size());
 	}
 
 }
